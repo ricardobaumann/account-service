540b748709eb8e4c087874bf74300805e0961dce
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-08-23 11:30:38 +0200

    auto invest wip

--- src/main/java/com/github/ricardobaumann/accountservice/repos/AutoInvestSettingsRepo.java
+++ src/main/java/com/github/ricardobaumann/accountservice/repos/AutoInvestSettingsRepo.java
@@ -9,4 +9,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 @RepositoryRestResource
 @PreAuthorize("hasRole('ROLE_USER')")
 public interface AutoInvestSettingsRepo extends CrudRepository<AutoInvestSetting,Long> {
+    /*
+    possibilities
+
+    1. publish account and auto invest changes into a kafka topic
+    2. listens for those events on loan-service to create investments
+     */
 }
